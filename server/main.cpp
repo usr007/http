5622583781fdc62d993359323de916fdb97f560a
usr007 <920930921@qq.com>
2018-02-01 13:55:17 +0800

    udpate startup parameter

--- server/main.cpp
+++ server/main.cpp
@@ -13,15 +13,15 @@
 #include <asio.hpp>
 #include "server.hpp"
 
-// 127.0.0.1 80 ../doc_root
+// 127.0.0.1 80
 int main(int argc, char* argv[])
 {
   try
   {
     // Check command line arguments.
-    if (argc != 4)
+    if (argc != 3)
     {
-      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
+      std::cerr << "Usage: http_server <address> <port>\n";
       std::cerr << "  For IPv4, try:\n";
       std::cerr << "    receiver 0.0.0.0 80 .\n";
       std::cerr << "  For IPv6, try:\n";
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
     }
 
     // Initialise the server.
-    http::server::server s(argv[1], argv[2], argv[3]);
+    http::server::server s(argv[1], argv[2], "./doc_root");
 
     // Run the server until stopped.
     s.run();
